71b3e5d836a4401818cddccd85742a9e817657b6
wooooozin <wooooozin@gmail.com>
2022-05-10 08:27:30 +0900

    asset 추가 및 kakao 계정 로그인

---
+++ MovieIt/Login/ViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  MovieIt
+//
+//  Created by 효우 on 2022/05/09.
+//
+
+import UIKit
+import Alamofire
+import KakaoSDKUser
+import KakaoSDKAuth
+import KakaoSDKCommon
+
+class ViewController: UIViewController {
+
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // 유효한 토큰 검사
+        if (AuthApi.hasToken()) {
+            UserApi.shared.accessTokenInfo { (_, error) in
+                if let error = error {
+                    if let sdkError = error as? SdkError, sdkError.isInvalidTokenError() == true  {
+                        //로그인 필요
+                    }
+                    else {
+                        //기타 에러
+                    }
+                }
+                else {
+                    //토큰 유효성 체크 성공(필요 시 토큰 갱신됨)
+                    
+                    // 사용자 정보를 가져오고 화면전환을 하는 커스텀 메서드
+                    self.getUserInfo()
+                }
+            }
+        }
+        else {
+            //로그인 필요
+        }
+    }
+    
+    //MARK: - Actions
+
+    @IBAction func didTapKakaoBtn(_ sender: UIButton) {
+        loginKakaoAccount() 
+    }
+    
+    @IBAction func didTapGuestBtn(_ sender: UIButton) {
+        
+    }
+    
+    
+    //MARK: - Kakao Login Methods
+//    func loginKakao() {
+//          print("loginKakao() called.")
+//          // 카카오톡 설치 여부 확인
+//          if (UserApi.isKakaoTalkLoginAvailable()) {
+//              UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+//                  if let error = error {
+//                      print(error)
+//                  }
+//                  else {
+//                      print("loginWithKakaoTalk() success.")
+//
+//                      // 회원가입 성공 시 oauthToken 저장가능하다
+//                      // _ = oauthToken
+//
+//                      // 사용자정보를 성공적으로 가져오면 화면전환 한다.
+//                      self.getUserInfo()
+//                  }
+//              }
+//          }
+//          // 카카오톡 미설치
+//          else {
+//              print("카카오톡 미설치")
+//          }
+//      }
+    
+    func loginKakaoAccount() {
+        print("loginKakaoAccount() called.")
+        
+        // 기본 웹 브라우저를 사용하여 로그인 진행.
+        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("loginWithKakaoAccount() success.")
+                
+                // 회원가입 성공 시 oauthToken 저장
+                 _ = oauthToken
+                
+                // 사용자정보를 성공적으로 가져오면 화면전환 한다.
+                self.getUserInfo()
+            }
+        }
+    }
+    
+    
+}
+
+// MARK: - Extensions
+
+extension ViewController {
+
+//  사용자 정보를 성공적으로 가져오면 화면전환 한다.
+    private func getUserInfo() {
+
+        // ✅ 사용자 정보 가져오기
+        UserApi.shared.me() {(user, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("me() success.")
+                
+                // ✅ 닉네임, 이메일 정보
+                let nickname = user?.kakaoAccount?.profile?.nickname
+                let email = user?.kakaoAccount?.email
+                
+                
+                let vc = MypageViewController.init(nibName: "MypageViewController", bundle: nil)
+                vc.email = email
+                vc.nickname = nickname
+                vc.modalPresentationStyle = .overFullScreen
+               
+                self.present(vc, animated: true, completion: nil)
+            }
+        }
+    }
+}
---
+++ MovieIt/MyPage/MypageViewController.swift
@@ -0,0 +1,84 @@
+//
+//  MainViewController.swift
+//  MovieIt
+//
+//  Created by 효우 on 2022/05/10.
+//
+
+import UIKit
+import KakaoSDKUser
+import KakaoSDKAuth
+import KakaoSDKCommon
+
+class MypageViewController: UIViewController {
+    
+    
+    var email: String?
+    var nickname: String?
+    
+    @IBOutlet weak var nickNameLbl: UILabel!
+    @IBOutlet weak var emailLbl: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+    }
+    
+    //MARK: - Actions
+    
+    @IBAction func didTapLogOut(_ sender: UIButton) {
+        
+        // 로그아웃 : 로그아웃은 API 요청의 성공 여부와 관계없이 토큰을 삭제 처리한다는 점에 유의합니다.
+        UserApi.shared.logout {(error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("logout() success.")
+                
+                // 로그아웃 시 메인으로 보냄
+                self.view.window?.rootViewController?.dismiss(animated: true)
+
+                }
+            }
+        }
+    
+    
+    @IBAction func didTapUnlinked(_ sender: UIButton) {
+        // 연결 끊기 : 연결이 끊어지면 기존의 토큰은 더 이상 사용할 수 없으므로, 연결 끊기 API 요청 성공 시 로그아웃 처리가 함께 이뤄져 토큰이 삭제됩니다.
+        UserApi.shared.unlink {(error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("unlink() success.")
+                
+                // 연결끊기 시 메인으로 보냄
+                self.view.window?.rootViewController?.dismiss(animated: true)
+
+                }
+            }
+        }
+    }
+
+// MARK: - Extensions
+    
+    extension MypageViewController {
+        private func setUI() {
+            // 사용자 정보 보여주기
+            if let nickname = nickname {
+                nickNameLbl.text = "\(nickname)님 환영합니다."
+            }
+            // 닉네임의 경우 필수동의 항목이라서 else 문이 필요하지 않았다.
+            
+            if let emial = email {
+                emailLbl.text = "\(emial)님 환영합니다."
+            } else {
+                emailLbl.text = "이메일 정보 수집을 동의하십시오."
+            }
+        }
+    }
+
